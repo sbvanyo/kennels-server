0af68e4b069d4f47685103d46948643971b4a0a2
Stacey Vanyo <sbvanyo@gmail.com>
2023-10-16 21:13:17 -0500

    customer, employee, location requests

---
+++ views/employee_requests.py
@@ -0,0 +1,25 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Jenna Solis"
+    }
+]
+
+def get_all_employees():
+    return EMPLOYEES
+
+
+# Function with a single parameter
+def get_single_employee(id):
+    # Variable to hold the found employee, if it exists
+    requested_location = None
+
+    # Iterate the EMPLOYEES list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for employee in EMPLOYEES:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if employee["id"] == id:
+            requested_employee = employee
+
+    return requested_employee
